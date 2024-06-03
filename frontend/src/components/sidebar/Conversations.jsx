ad57a7858909b5465eef644a1b697b0e90be08fd
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-06-03 21:34:38 +0530

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Conversation from './Conversation.jsx';
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    <Conversation /> 
+    <Conversation /> 
+    <Conversation /> 
+    <Conversation />
+    </div>
+  )
+}
+
+export default Conversations;
+
+
+//starter code snippet 
+// import React from 'react'
+// import Conversation from './Conversation.jsx';
+
+// const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//     <Conversation/>
+//     <Conversation/>
+//     <Conversation/>
+//     <Conversation/>
+//     <Conversation/>
+//     <Conversation/>
+//     </div>
+//   )
+// }
+
+// export default Conversations;
